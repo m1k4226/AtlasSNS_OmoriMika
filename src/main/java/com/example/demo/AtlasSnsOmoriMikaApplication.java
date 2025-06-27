5ce6312c6ba7da427ebb88b3d203786525ede3cc
m1k4226 <m1k4.m.o226@gmail.com>
2025-06-27 18:01:16 +0900

    first commit

---
+++ src/main/java/com/example/demo/AtlasSnsOmoriMikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasSnsOmoriMikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasSnsOmoriMikaApplication.class, args);
+	}
+
+}
