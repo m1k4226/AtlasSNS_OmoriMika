6eef186692ec54ba424281901e6a82aafd31c8a8
m1k4226 <m1k4.m.o226@gmail.com>
2025-07-01 16:18:07 +0900

    Entity,Repository追加

---
+++ src/main/java/com/example/demo/entity/Post.java
@@ -0,0 +1,119 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+	
+//	CREATE TABLE posts(
+//			id SERIAL PRIMARY KEY,
+//			user_id Integer NOT NULL,CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
+//			post varchar(400) NOT NULL,
+//			created_at timestamp NOT NULL DEFAULT current_timestamp,
+//			updated_at timestamp NOT NULL DEFAULT current_timestamp
+//		);
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User userId;
+	
+	@Column(name = "post")
+	private String content;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	
+	// 新規データ（INSERT）される直前に自動で呼ばれる
+	// ※userRepository.save(user)実行時にJPAにより実行
+	// createdAt と updatedAt の両方に現在時刻をセット
+//	@PrePersist
+//	public void onCreate() {
+//		LocalDateTime now = LocalDateTime.now();
+//		this.createdAt = now;
+//		this.updatedAt = now;
+//	}
+	
+	// 新規データ（INSERT）される直前に自動で呼ばれる
+	// ※userRepository.save(user)実行時にJPAにより実行
+	// updatedAt のみ更新
+//	@PreUpdate
+//	public void onUpdate() {
+//		this.updatedAt = LocalDateTime.now();
+//	}
+	
+	// デフォルトコンストラクタ
+	public Post() {
+		super();
+	}
+
+	// 新規投稿
+	public Post(User userId, String content) {
+		super();
+		this.userId = userId;
+		this.content = content;
+	}
+
+	// ゲッターセッター
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getUserId() {
+		return userId;
+	}
+
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+}
