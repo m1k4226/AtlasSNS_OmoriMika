aa849ad1a1117d191c5d20a9ec913d6a4d281345
m1k4226 <m1k4.m.o226@gmail.com>
2025-07-01 16:17:25 +0900

    sqlファイル追加

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+
+--ユーザーテーブル
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+	username varchar(255) NOT NULL,
+	email varchar(255) NOT NULL CONSTRAINT unique_email UNIQUE,
+	password varchar(255) NOT NULL,
+	bio varchar(400),
+	icon_image varchar(255) NOT NULL DEFAULT 'icon1.png',
+	created_at timestamp NOT NULL DEFAULT current_timestamp,
+	updated_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+--投稿テーブル
+CREATE TABLE posts(
+	id SERIAL PRIMARY KEY,
+	user_id Integer NOT NULL,
+	CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	post varchar(400) NOT NULL,
+	created_at timestamp NOT NULL DEFAULT current_timestamp,
+	updated_at timestamp NOT NULL DEFAULT current_timestamp
+);
+
+--フォローテーブル
+CREATE TABLE follows(
+	id SERIAL PRIMARY KEY,
+	following_id Integer NOT NULL,
+	followed_id Integer NOT NULL,
+	created_at timestamp NOT NULL DEFAULT current_timestamp,
+	updated_at timestamp NOT NULL DEFAULT current_timestamp
+);
